b6744e6bd998455bc540f4264ce2eb86a3f59b49
caodangkhoa600 <caodangkhoa600@gmail.com>
2026-05-27 13:18:25 +0700

    remote routing slip

--- 6.RoutingSlip/Contracts/OrderLog.cs
+++
@@ -1,8 +0,0 @@
-namespace Contracts;
-
-public record OrderLog
-{
-    public string OrderId { get; init; } = null!;
-    public string Step { get; init; } = null!;
-    public DateTimeOffset CompletedAt { get; init; }
-}
